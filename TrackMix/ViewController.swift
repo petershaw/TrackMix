cc1fbb16e19df520c9322f547607c996fea86afc
Kris <kris@opentabs.de>
2014-12-03 17:40:31 +0100

    Initial Commit

---
+++ TrackMix/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrackMix
+//
+//  Created by Kris Wolff on 03/12/14.
+//  Copyright (c) 2014 de.petershaw. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
