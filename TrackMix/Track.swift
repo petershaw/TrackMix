03ab9ebac98e552e55f01b2919b47fb892a15f5e
Kris <kris@opentabs.de>
2014-12-04 11:52:18 +0100

    add system volume control

--- TrackMix/Track.swift
+++ TrackMix/Track.swift
@@ -21,8 +21,38 @@ class Track {
     }
     
     func passValueToSystemVolume(value: Float){
-        //AudioQueueSetParameter(AudioQueueRef, kAudioQueueParam_Volume, 0.5);
+        var defaultOutputDeviceID = AudioDeviceID(0)
+        var defaultOutputDeviceIDSize = UInt32(sizeofValue(defaultOutputDeviceID))
+        
+        var getDefaultOutputDevicePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: AudioObjectPropertySelector(kAudioHardwarePropertyDefaultOutputDevice),
+            mScope: AudioObjectPropertyScope(kAudioObjectPropertyScopeGlobal),
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        let status1 = AudioObjectGetPropertyData(
+            AudioObjectID(kAudioObjectSystemObject),
+            &getDefaultOutputDevicePropertyAddress,
+            0,
+            nil,
+            &defaultOutputDeviceIDSize,
+            &defaultOutputDeviceID)
 
+        // Set volume on default output device:
+        var volume = Float32(value / 10) // 0.0 ... 1.0
+        var volumeSize = UInt32(sizeofValue(volume))
+        
+        var volumePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: AudioObjectPropertySelector(kAudioHardwareServiceDeviceProperty_VirtualMasterVolume),
+            mScope: AudioObjectPropertyScope(kAudioDevicePropertyScopeOutput),
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        let status2 = AudioHardwareServiceSetPropertyData(
+            defaultOutputDeviceID,
+            &volumePropertyAddress,
+            0,
+            nil,
+            volumeSize,
+            &volume)
     }
     
 }
